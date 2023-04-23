0db2be177dd72424970ca1da0570910762ec8410
Niveditajeyakumar <131595739+Niveditajeyakumar@users.noreply.github.com>
2023-04-23 18:30:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.title('Omega 3 and Blueberry Oatmeal')
+streamlit.title('Kale,Spinach and Rocket Smoothie')
+streamlit.title('Hard-Boiled Free Range Egg')
